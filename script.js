8b3a9620e57d8a57c43070696a27a5f71ad72bf0
nesbita <ariananesbit@gmail.com>
2021-05-17 17:58:59 -0600

    NW

--- script.js
+++ script.js
@@ -7,20 +7,26 @@ function plusSlides(n) {
 }
 
 function showSlides(n) {
-    let i 
-    let slides=document.getElementsByClassName("gSlide")
-console.log(slides)
-const slideArray = Array.from(slides)
-console.log(slideArray)
-
+    // let i 
+    let slides=document.getElementsByClassName("gSlide");
+// console.log("SLIDES WITH CLASS NAME GSLIDE", slides)
+// const slideArray = Array.from(slides)
+// console.log("ARRAY OF SLIDES", slideArray)
     if (n > slides.length) {slideIndex=1}
     if (n < 1) {slideIndex=slides.length}
-    for (let i=0; i < slides.length; i++) {
-        slides[i].style.display="none"; 
+    for (let i=0; i < slides.length; i++) 
+    {
+        // console.log("SLIDES AT I", slides[i])
+        // slides[i].style.display="none"; 
     }
+    
     slides[slideIndex-1].style.display = "block";
-    setTimeout (showSlides, 2000);
+    // setTimeout (showSlides, 2000);
     }
 
 
+// let slides=document.getElementsByClassName("gSlide")
+// console.log(slides[0])
+// slides[0].style.display="block"
+// console.log(slides)
 
